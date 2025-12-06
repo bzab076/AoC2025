47e13f1ee9aaa3cc2a9ac85771382bb47cac0f9d
bzab076 <bzab076@gmail.com>
2025-12-06 10:44:54 +0100

    Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,36 @@
+@Suppress("unused")
+class Day06 : AbstractDay(6) {
+
+    //private val nums = inputLines().dropLast(1).map{it.extractNumbers()}
+    private val lines = inputLines().dropLast(1).map { "$it " }
+    private val cols = getColumns(emptyList(), lines)
+    private val ops = inputLines().last().split(" ").map { it.trim() }.filter { it.isNotEmpty() }.map { it[0] }
+
+    private fun operation(op : Char, vals : List<Int>) : Long =
+        when (op) {
+            '+' -> vals.sumOf { it.toLong() }
+            '*' -> vals.map{it.toLong()}.fold(1){ acc, v -> acc*v}
+            else -> 0L
+        }
+
+    private tailrec fun getColumns (acc : List<List<String>>, lines : List<String>) : List<List<String>> {
+
+        if(lines.isEmpty() || lines.any { it.isEmpty() }) return acc
+
+        val maxlen = lines.map { it.indexOfFirst { it==' ' } }.max()
+        val (cols, tails) = lines.map{it.substring(0,maxlen) to  it.substring(maxlen+1)}.unzip()
+        return getColumns(acc + listOf(cols), tails)
+    }
+
+    private fun transposeNumStrings(vals : List<String>) : List<String> {
+        val maxlen = vals.maxOf { it.length }
+        return (0..<maxlen).map { idx -> vals.map { it[idx] }.joinToString("")}
+    }
+
+    override fun partOne(): Any = cols.mapIndexed { idx, v ->  operation(ops[idx],  v.map { it.trim().toInt() } ) }.sum()
+
+    override fun partTwo(): Any {
+        val transposed = cols.map { transposeNumStrings(it) }.map { it.map { it.trim().toInt() } }
+        return transposed.mapIndexed { i, v -> operation(ops[i], v) }.sum()
+    }
+}
